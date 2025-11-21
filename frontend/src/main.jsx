c450013ebb15855785f14a17bd417e02aad3b42b
CiberNinja <ciberninja2025@gmail.com>
2025-11-21 16:20:05 +0000

    test-hospedagem-09

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/Aplicacao-GUI-de-Email">
+    <BrowserRouter basename="/Aplicacao-GUI-de-Email">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
